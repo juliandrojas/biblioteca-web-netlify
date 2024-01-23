39907bba5b2ffaed15ea30f59a01fd9385e094a9
Julian Rojas <juliandra140201@gmail.com>
2024-01-23 18:08:42 -0500

    Add("Base of components")

---
+++ src/pages/Index.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+const Index = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://biblioteca-api-lt9e.onrender.com/api/ping');
+        if (!response.ok) {
+          throw new Error('Error en la solicitud a la API');
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {loading && <p>Cargando...</p>}
+      {error && <p>Error: {error}</p>}
+      {data && (
+        <div>
+          <h2>Respuesta de la API:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Index;
